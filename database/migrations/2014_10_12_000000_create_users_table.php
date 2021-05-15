6d8bfee9086a2dbdb0280ae9bbe349a94bc3747a
William de León <68469930+Sinder20@users.noreply.github.com>
2021-05-15 07:59:34 -0600

    Creacion de Login y Registro de Usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('tipo_usuario')->default(0); // 0 = suscriptor && 1 = administrador
             $table->rememberToken();
             $table->timestamps();
         });
